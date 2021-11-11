d8055e6df77b875acb81f5f481138fc8d9c84843
Goodnews Ike <goodnewsike19@gmail.com>
2021-11-11 09:05:08 +0100

    updating other projects section

--- src/_sections/Project/OtherProjects.js
+++ src/_sections/Project/OtherProjects.js
@@ -3,30 +3,35 @@ import React from 'react';
 const OtherProjects = (props) => {
    return (
       <>
-         <div key={props.id} className="col-md-4 col-12 mb-md-5">
-            <div className="inner position-relative">
-               <div className="img">
-                  <img src={props.projectImg} alt="" className="" />
-               </div>
-               <div className="description position-absolute">
-                  <div className="d-flex align-items-center">
-                     {props.stack.map((item) => (
-                        <span key={item.id} className="stack">
-                           {item.name}
-                        </span>
-                     ))}
+         <div key={props.id} className="col-md-4 col-12 mb-md-5 mb-3">
+            <a href="!#" className="">
+               <div className="inner position-relative">
+                  <div className="img">
+                     <img src={props.projectImg} alt="" className="" />
                   </div>
-                  <p className="">{props.description}</p>
-                  <div className="icons">
-                     <a href={props.githubLink} className="">
-                        {props.githubIcon}
-                     </a>
-                     <a href={props.extUrl} className="">
-                        {props.extIcon}
-                     </a>
+                  <div className="description h-100 p-4 position-absolute d-flex align-items-center justify-content-center">
+                     <div className="position-relative">
+                        <h5 className="title pb-2 text-capitalize">project title</h5>
+                        <p className="pt-2 pb-3">{props.description}</p>
+                        <div className="project-stack pb-4 d-flex align-items-center">
+                           {props.stack.map((item) => (
+                              <span key={item.id} className="stack me-2">
+                                 {item.name}
+                              </span>
+                           ))}
+                        </div>
+                        <div className="icons">
+                           <a href={props.githubLink} className="me-4">
+                              {props.githubIcon}
+                           </a>
+                           <a href={props.extUrl} className="">
+                              {props.extIcon}
+                           </a>
+                        </div>
+                     </div>
                   </div>
                </div>
-            </div>
+            </a>
          </div>
       </>
    );
